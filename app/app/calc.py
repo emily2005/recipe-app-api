636e4e705ca229e9f6ac783786086a008001ba70
Emily Wright <emily.wright@ul.org>
2024-10-10 15:26:18 -0700

    Fix white space

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calc func
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add x and y and return results"""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """subtract x from y and return result"""
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,14 +6,21 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
+
     """Test calc module"""
+
     def test_add_numbers(self):
         """Test adding numers together by running thru the calc add function"""
-        res = calc.add(5 ,6)
+
+        res = calc.add(5, 6)
+
         self.assertEqual(res, 11)
 
     def test_susbtract_numbers(self):
         """Test subtracting stuff"""
-        res = calc.subtract (10, 15)
+
+        res = calc.subtract(10, 15)
+
         self.assertEqual(res, 5)
