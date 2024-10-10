b40a7ef15a3b1829a81a9c3e0c91080e44fcf028
Emily Wright <emily.wright@ul.org>
2024-10-10 15:18:27 -0700

    Add postgres connection

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calc func
+"""
+
+def add(x,y):
+    """Add x and y and return results"""
+    return x + y
+
+def subtract(x,y):
+    """subtract x from y and return result"""
+    return y - x
