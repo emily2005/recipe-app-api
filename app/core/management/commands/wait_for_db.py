234acc7d17981c738926e43df5ecd63369c55f80
Emily Wright <emily.wright@ul.org>
2024-10-11 12:31:16 -0700

    Add test code

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,12 +1,34 @@
 """
 Django command to wait for database to be available
 """
+import time
 
-from django.core.management.base import BaseCommand
+from psycopg2 import OperationalError as Psycopg2Error
+
+"""Error Django throws when db is not ready"""
+from django.db.utils import OperationalError  # noqa: E402
+from django.core.management.base import BaseCommand  # noqa: E402
 
 
 class Command(BaseCommand):
     """Django command to wait for db"""
 
     def handle(self, *args, **options):
-        pass
+        """Entrypoint for command"""
+        self.stdout.write('waiting for db...')
+        """assume db is not up until we know for sure"""
+        db_up = False
+        """while db_up is false well do the try/catch and tell us it is not
+        ready"""
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                """if we get Psycoppg2Error or Operational error print to
+                screen"""
+                self.stdout.write('db unavail, waiting...')
+                time.sleep(1)
+
+        """self.style.SUCCESS gives a nice green output"""
+        self.stdout.write(self.style.SUCCESS('db up and avail'))
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -8,3 +8,32 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands"""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for db if db is up and running"""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+
+    """want to sleep between checking db so we dont keep checking
+    while startng"""
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for db when db gives error"""
+        """First two times, call the mocked method to raise psycopg2 error"""
+        """Next three times raise operational error"""
+        """6th time return True"""
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
