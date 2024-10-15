6426a1d59bec47abb49c4d490965176f81c6033e
Emily Wright <emily.wright@ul.org>
2024-10-15 13:29:23 -0700

    appease flake8

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,16 +10,16 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating user with email success"""
-        email ='test@example.com'
+        email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-        
+
     def test_new_user_email_normalized(self):
         """Test email lis normalized for new user"""
         """define list below, a list of list first element is the email second
@@ -31,7 +31,7 @@ class ModelTests(TestCase):
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
-            user=get_user_model().objects.create_user(email, 'sample123')
+            user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_error(self):
